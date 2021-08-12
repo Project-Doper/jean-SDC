f5d486e20638a02f2660ca9864216ba01ad3aad6
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-08-12 14:43:56 -0700

    Refactored config.js, index.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,35 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 // const mongoUri = 'mongodb://localhost/blogmodo';
 
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+// mongoose.connect(mongoUri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
+
+// const db = mongoose.connection;
+
+// module.exports = db;
+
+const config = require("./config/config.js");
+
+const { Pool, Client } = require("pg");
+
+const pool = new Pool({
+  user: config.user,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+  port: config.port,
 });
 
-const db = mongoose.connection;
+const client = new Client({
+  user: config.user,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+  port: config.port,
+});
 
-module.exports = db;
+exports.pool = pool;
+exports.client = client;
--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const path = require("path");
 const router = require("./router");
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/../client/dist")));
+// app.use(express.static(path.join(__dirname, "/../client/dist")));
 app.use("/api", router);
 
 app.listen(port, (err) => {
